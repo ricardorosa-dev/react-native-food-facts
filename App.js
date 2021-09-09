d23f8e46919c838f9e021de3b6d946f1f6e7507a
Ricardo Rosa <64689464+ricardorosa-dev@users.noreply.github.com>
2021-09-08 22:13:02 -0300

    Subindo a v. 1 do aplicativo pronta.

--- App.js
+++ App.js
@@ -2,7 +2,7 @@ import React, { useState } from 'react';
 import { Text, View, TextInput } from 'react-native';
 import { NativeRouter, Route } from 'react-router-native';
 
-import Home from './Home';
+import Home from './pages/Home';
 import FoodItem2 from './pages/FoodItem2';
 
 export default function App() {
